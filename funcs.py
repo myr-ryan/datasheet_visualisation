c7366d38a9a8081e011e597a1b4f60ea729b1c8b
Yingrui Ma <yingrui.ma21@imperial.ac.uk>
2023-04-03 16:54:31 +0100

    make it plot 2 vars

--- funcs.py
+++ funcs.py
@@ -2,25 +2,87 @@ import pandas as pd
 import numpy as np
 
 
-# Inputs:
-#       df: dataframe from excel file
-#       
-def preprocess(df, filter_values):
-    
-    # Only keep the rows that have been recorded
-    df = df.dropna(subset=["ml_task_description", "data_units", "data_size_all", "raw data availability"], how='all')
+def type_conversion(df):
+    def astype_conv(df, column_name, conv_type):
+        try:
+            df = df.astype({column_name: conv_type})
+        except:
+            print("Please check the name of column. It should be: ", column_name)
+        return df
 
-    # for f in filters:
-    #     filter_values.append(list(set(df[f].tolist())))
-
-    # TODO
-    filter_values.append(list(set(df['data_units'].tolist())))
-    filter_values.append(list(set(df['raw data availability'].tolist())))
-    filter_values.append(list(set(df['processed data availability'].tolist())))
-    
-    print(len(filter_values[0]))
-    print(filter_values) 
-
-    
+    df = astype_conv(df, "Paper ID", int)
+    df = astype_conv(df, "Title", str)
+    df = astype_conv(df, "ml_task_description", int)
+    df = astype_conv(df, "data_units", str)
+    df = astype_conv(df, "data_size_all", float)
+    df = astype_conv(df, "data_size_validation", float)
+    df = astype_conv(df, "data_size_testing", float)
+    df = astype_conv(df, "data_size_training", float)
+    df = astype_conv(df, "explainability", str)
+    df = astype_conv(df, "balanced", bool)
+    df = astype_conv(df, "balanced_comment", str)
+    df = astype_conv(df, "bias", str)
+    df = astype_conv(df, "class_labels", str)
+    df = astype_conv(df, "data_source", str)
+    df = astype_conv(df, "data_links", str)
+    df = astype_conv(df, "raw data availability", int)
+    df = astype_conv(df, "processed data availability", int)
+    df = astype_conv(df, "gender", int)
+    df = astype_conv(df, "age specified", str)
+    df = astype_conv(df, "task_detection", bool)
+    df = astype_conv(df, "task_diagnosis", bool)
+    df = astype_conv(df, "task_prognosis", bool)
+    df = astype_conv(df, "treatment design (monitoring, planning)", bool)
+    df = astype_conv(df, "treatment design comment", str)
+    df = astype_conv(df, "task_risk prediction", bool)
+    df = astype_conv(df, "task_subtyping", bool)
+    df = astype_conv(df, "task_other ( specify)", str)
+    df = astype_conv(df, "subspec_ovarian", bool)
+    df = astype_conv(df, "subspec_cervical", bool)
+    df = astype_conv(df, "subspec_endometrial", bool)
+    df = astype_conv(df, "subspec_metastasis", bool)
+    df = astype_conv(df, "subspec_other (specify)", bool)
+    df = astype_conv(df, "performance_auc", float)
+    df = astype_conv(df, "performance_precision (PPV)", float)
+    df = astype_conv(df, "performance_specificity", float)
+    df = astype_conv(df, "performance_NPV", float)
+    df = astype_conv(df, "performance_sensitivity (recall)", float)
+    df = astype_conv(df, "performance_F1", float)
+    df = astype_conv(df, "performance_accuracy", float)
+    df = astype_conv(df, "neural network type", str)
+    df = astype_conv(df, "data collection technology", str)
+    df = astype_conv(df, "algorithm-pipeline", str)
+    df = astype_conv(df, "sample type", str)
 
     return df
+
+
+# Inputs:
+#       df: dataframe from excel file
+# Outputs:
+#       df: dataframe after preprocessing
+#       filter_values: in form list of list. contains selectable values from all filters
+def preprocess(df):
+    
+    column_names = list(df.columns.values)
+    # Only keep the rows that have been recorded
+    # Criteria: for a specific row, if all the values except column "Title" is not empty,
+    #           then it means this row has been recorded.
+    df = df.dropna(subset=list(set(column_names)-set(["Title"])), how='all')
+    df = type_conversion(df)
+
+
+    # TODO
+    
+    filter_values = []
+    filter_values.append(list(set(df['data_units'].tolist())))
+    filter_values[0].insert(0, "All")
+    # filter_values.append(list(set(df['raw data availability'].tolist())))
+    # filter_values.append(list(set(df['processed data availability'].tolist())))
+    
+    # print(len())
+    # print(filter_values) 
+
+    
+
+    return df, filter_values
--- newtry.py
+++ newtry.py
@@ -1,6 +1,6 @@
 from bokeh.io import curdoc
 from bokeh.models.widgets import FileInput
-from bokeh.models import ColumnDataSource, DataTable, TableColumn, Button, ColorPicker
+from bokeh.models import ColumnDataSource, Button, ColorPicker, HoverTool
 from bokeh.models.widgets import Slider, TextInput, Select
 from bokeh.layouts import column, row
 from bokeh.plotting import figure
@@ -13,12 +13,14 @@ import pandas as pd
 import numpy as np
 
 # First create an empty plot
-empty_data = {'Paper ID':[],
+empty_data = {'x':[],
               'y':[]}
 source = ColumnDataSource(data=empty_data)
+source_backup = ColumnDataSource(data=empty_data)
+
 # x_range=source.data["Paper ID"]
-plot = figure(height=400, width=500, title='Datasheet visualization')
-plot.scatter('Paper ID', 'y', source=source)
+plot = figure(height=400, width=500, title='Datasheet visualization', x_axis_label="Var1", y_axis_label="Var2")
+plot.scatter('x', 'y', source=source)
 # scatter = Scatter(x="Paper ID", y="y", marker="circle")
 # plot.add_glyph(source, scatter)
 
@@ -26,56 +28,108 @@ plot.scatter('Paper ID', 'y', source=source)
 # file_input = FileInput(accept='.xlsx', width=400)
 file_input = FileInput(accept='.xlsx')
 
-# All plotable variables from the file, hard coded
-plotable = ['data_size_all', 'performance_auc', 'performance_precision (PPV)', 'performance_specificity', 
-            'performance_NPV', 'performance_sensitivity (recall)', 'performance_F1', 'performance_accuracy']
+# All plotable variables from the file, hard coded for now
+# plotable = ['data_size_all', 'performance_auc', 'performance_precision (PPV)', 'performance_specificity', 
+            # 'performance_NPV', 'performance_sensitivity (recall)', 'performance_F1', 'performance_accuracy']
 
-# plotable.insert(0, 'All')
 
-# Filters that you can apply on the plotting results, hard coded
+# Filters that you can apply on the plotting results, hard coded for now
 filters = ['data_units', 'raw data availability', 'processed data availability', 'task', 'subspec', 'sample type']
 
 # list to store all selectable values in the filters
-filter_values = []
+
+
 # filter0 = ['images', 'patients', 'cases', 'text']
 # filter0.insert(0, 'All')
 
 
+# Set up widgets for variables that need to be plotted, and filters to apply
+var_1 = Select(title="Please select the 1st variable", value="select", options=[])
+var_2 = Select(title="Please select the 2nd variable", value="select", options=[])
+
+
+filter0_widget = Select(title=filters[0], value="All", options=[])
 
 # Callback for file_input
 def upload_data(attr, old, new):
+    # Read excel file into dataframe
     decoded = base64.b64decode(new)
     f = io.BytesIO(decoded)
     df = pd.read_excel(f, sheet_name='Sheet1', engine='openpyxl')
-    df = preprocess(df, filter_values)
+
+    # Function 'preprocess' in file funcs.py
+    # Returns the correct data types in each column and the possible selections of filters
+    df, filter_values = preprocess(df)
+
+    # Update selection widget: variables to plot
+    numeric_var = list(df.select_dtypes(include='float64').columns.values)
+    numeric_var.insert(0, "select")
+    var_1.options = numeric_var
+    var_2.options = numeric_var
+
+    # Update selection widget: filters
+    filter0_widget.options = filter_values[0]
     source.data = df
+    source_backup.data = df
     print('Dataset uploaded successfully')
 
 # Callback for button_apply
 def update_plot():
-    var_value = str(plot_var.value)
-    filter0 = str(filter0_widget.value)
-    df = source.data  
-    df = pd.DataFrame(df)
-    selected = df[df[filters[0]] == filter0]
-    x = selected["Paper ID"]
-    y = selected[var_value]
-    # source.data = dict(x=x, y=y)
-    source.data = {"Paper ID": x, "y": y}
-    # plot = figure(height=400, width=500, title='Datasheet visualization')
-    # scatter = Scatter(x="Paper ID", y=var_value, marker="circle")
-    # plot.add_glyph(source, scatter) 
+    plot_var_1 = str(var_1.value)
+    plot_var_2 = str(var_2.value)
+
+    if plot_var_1 == "select" or plot_var_2 == "select":
+        # text_warning = Text(x="x", y="y", text="Please select your variables to plot")
+        # plot.title = "Please select your variables to plot!!"
+        button_apply.label = "Please re-select!"
+        button_apply.button_type = "danger"
+        # text_warning = Text()
+        # plot.add_layout(text_warning)
+    
+    else:
+        button_apply.label = "Generate"
+        button_apply.button_type = "primary"
+        filter0 = str(filter0_widget.value)
+        # plot.y_axis_label = var_value
+        if filter0 == "All":
+            selected = pd.DataFrame(source_backup.data)
+        else:
+            df = pd.DataFrame(source_backup.data)
+            selected = df[df[filters[0]] == filter0]
+    
+
+        x = selected[plot_var_1]
+        y = selected[plot_var_2]
+        plot.x_range.start = x.min()
+        plot.x_range.end = x.max()
+        plot.y_range.start = y.min()
+        plot.y_range.end = y.max()
+        plot.xaxis.axis_label = plot_var_1
+        plot.yaxis.axis_label = plot_var_2
+
+        # TODO: need to retain all columns after filtering
+
+        plot.scatter(x, y, source=source)
+
+        
+        # source.add(pd.DataFrame([{"x": x, "y": y}]))
+        # source.data = {"x": x, "y": y}
+
+        # Create hover tool
+        # print(source.data)
+        hover = HoverTool(tooltips=[("Paper ID", "@{Paper ID}")])
+        plot.add_tools(hover)
+
+
 
 
 file_input.on_change('value', upload_data)
 
 
-# Set up widgets for variables that need to be plotted, and filters to apply
-plot_var = Select(title="Variable to plot", value=plotable[0], options=plotable)
-# filter0_widget = Select(title=filters[0], value="All", options=filter0)
+
 
 # Create a button widget for filters
-button_apply = Button(label="Apply", button_type="primary")
+button_apply = Button(label="Generate", button_type="primary")
 button_apply.on_click(update_plot)
 
 # # Create a color picker widget
@@ -84,10 +138,10 @@ button_apply.on_click(update_plot)
 
 
 
-# widgets = row(plot_var, filter0_widget)
-widgets = row(plot_var)
+widgets = row(filter0_widget)
+# widgets = row(plot_var)
 
-curdoc().add_root(column(file_input, widgets, button_apply, plot))
+curdoc().add_root(row(column(file_input, row(var_1, var_2), widgets, button_apply), plot))
 
 
 
