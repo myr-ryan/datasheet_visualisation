efe53f71f1e1dfaf5ed1ad1fb39e9711db936331
Yingrui Ma <yingrui.ma21@imperial.ac.uk>
2023-04-25 16:57:30 +0100

    finish filter function
    next step: apply min and max for numeric variables

--- Plot_Data.py
+++ Plot_Data.py
@@ -8,28 +8,39 @@ class Plot_Data:
     column_names = []
     filter_list = []
     value_list = []
-    filter_value_list = []
+    # filter_value_list = []
     task_values = []
     subspec_values = []
     numeric_var = []
+    bool_list = []
+    categ_list = []
 
 
     def __init__(self, data):
         self.source = ColumnDataSource(data=data)
         self.source_backup = ColumnDataSource(data=data)
 
+    
+    def is_equal_two_list(self, list1, list2):
+
+        if len(list1) == len(list2):
+            is_same = all([x == y for x, y in zip(list1, list2)])
+        else:
+            is_same = False
+        return is_same
+
     def get_column_from_name(self, df, column_name):
         column_data = np.unique(df[column_name].tolist())
-        column_data_no_nan = [str(x) for x in column_data if str(x) != 'nan']
+        column_data_no_nan = [x for x in column_data if str(x) != 'nan']
         return column_data_no_nan
 
 
     def type_conversion(self, df):
         for c in self.column_names:
             column_data_no_nan = self.get_column_from_name(df, c)
-            # if is_equal_two_list(column_data_no_nan, [0., 1.]):
-            #     df = df.astype({c: bool})
-            if len(column_data_no_nan) >=2 and len(column_data_no_nan) <= 7:
+            if self.is_equal_two_list(column_data_no_nan, [0., 1.]):
+                df = df.astype({c: bool})
+            elif len(column_data_no_nan) >=2 and len(column_data_no_nan) <= 7:
                 df = df.astype({c: 'category'})
             elif df[c].dtype == 'object':
                 df = df.astype({c: 'string'})
@@ -44,32 +55,33 @@ class Plot_Data:
         self.source.data = df
         self.source_backup.data = df
 
-    def add_indent(self, list):
-        res = []
-        for l in list:
-            res.append("*   " + str(l))
-        return res
+    # deprecated
+    # def add_indent(self, list):
+    #     res = []
+    #     for l in list:
+    #         res.append("*   " + str(l))
+    #     return res
 
-    def get_value_list(self):
-        df = pd.DataFrame(self.source.data)
+    # # deprecated
+    # def get_value_list(self):
+    #     df = pd.DataFrame(self.source.data)
+    #     for f in self.filter_list:
+    #         # self.filter_value_list.append(f)
+    #         if f == '(select)':
+    #             values_with_indent = []
+    #         elif f == 'tasks':
+    #             self.value_list.append(self.task_values)
+    #             values_with_indent = self.add_indent(self.task_values)          
+    #         elif f == 'subspec':
+    #             self.value_list.append(self.subspec_values)
+    #             values_with_indent = self.add_indent(self.subspec_values)
+    #         else:
+    #             values = self.get_column_from_name(df, f)
+    #             self.value_list.append(values)
+    #             values_with_indent = self.add_indent(values)
 
-        for f in self.filter_list:
-            self.filter_value_list.append(f)
-            if f == '(select)':
-                values_with_indent = []
-            elif f == 'tasks':
-                self.value_list.append(self.task_values)
-                values_with_indent = self.add_indent(self.task_values)          
-            elif f == 'subspecialities':
-                self.value_list.append(self.subspec_values)
-                values_with_indent = self.add_indent(self.subspec_values)
-            else:
-                values = self.get_column_from_name(df, f)
-                self.value_list.append(values)
-                values_with_indent = self.add_indent(values)
-
-            for v in values_with_indent:
-                self.filter_value_list.append(v)
+    #         for v in values_with_indent:
+    #             self.filter_value_list.append(v)
 
 
 
@@ -80,21 +92,22 @@ class Plot_Data:
         df = self.type_conversion(df)
         self.source.data = df
 
-        all_categorical = df.select_dtypes(include=['category']).columns.tolist()
-        self.task_values = [x for x in all_categorical if x.startswith('task')]
-        for f in self.task_values:
-            df = df.astype({f: 'bool'})
-        self.subspec_values = [x for x in all_categorical if x.startswith('subspec')]
-        for f in self.subspec_values:
-            df = df.astype({f: 'bool'})
-        
-        self.filter_list = df.select_dtypes(include=['category']).columns.tolist()
-        self.filter_list.insert(0, 'subspecialities')
-        self.filter_list.insert(0, 'tasks')
+        self.bool_list = df.select_dtypes(include=['bool']).columns.tolist()
+
+
+        # all_categorical_bool = df.select_dtypes(include=['category', 'bool']).columns.tolist()
+        self.task_values = [x for x in self.bool_list if x.startswith('task')]
+        self.subspec_values = [x for x in self.bool_list if x.startswith('subspec')]
+        self.bool_list = [x for x in self.bool_list if (not x.startswith('task')) and (not x.startswith('subspec'))]
+        self.bool_list.insert(0, 'subspec')
+        self.bool_list.insert(0, 'tasks')
+
+        self.categ_list = df.select_dtypes(include=['category']).columns.tolist()
+        self.filter_list = self.categ_list + self.bool_list
         self.filter_list.insert(0, "(select)")
         self.filter_list.sort()
 
-        self.get_value_list()
+        # self.get_value_list()
 
         self.numeric_var = df.select_dtypes(include=['float']).columns.tolist()
         self.numeric_var.insert(0, "(select)")
@@ -104,9 +117,10 @@ class Plot_Data:
     def debug_printing(self):
         # print(self.column_names)
         print(self.filter_list)
-        print(self.value_list)
-        print(self.filter_value_list)
-        # print(self.task_values)
-        # print(self.subspec_values)
+        # print(self.value_list)
+        print(self.bool_list)
+        # print(self.filter_value_list)
+        print(self.task_values)
+        print(self.subspec_values)
         # print(self.numeric_var)
 
--- newtry.py
+++ newtry.py
@@ -18,50 +18,20 @@ empty_data = {'x':[],
               'y':[]}
 
 scatter_plot_data = Plot_Data(empty_data)
-# source = ColumnDataSource(data=empty_data)
-# source_backup = ColumnDataSource(data=empty_data)
-
-# x_range=source.data["Paper ID"]
 plot = figure(height=400, width=500, title='Datasheet visualization', tooltips=None)
 plot.scatter('x', 'y', source=scatter_plot_data.source)
-# scatter = Scatter(x="Paper ID", y="y", marker="circle")
-# plot.add_glyph(source, scatter)
 
-
-# file_input = FileInput(accept='.xlsx', width=400)
-file_input = FileInput(accept='.xlsx')
-
-
-# Filters that you can apply on the plotting results, hard coded for now
-# filters = ['data_units', 'raw data availability', 'processed data availability', 'task', 'subspec', 'sample type']
-
+upload_prompt = Div(text='''Please upload your datasheet''')
+file_input = FileInput(accept='.xlsx', width=500, height=40, margin=(0,0,25,0))
 
 # Set up widgets for variables that need to be plotted, and filters to apply
-var_1 = Select(title="Please select var on x axis", value="(select)", options=[])
-var_2 = Select(title="Please select var on y axis", value="(select)", options=[])
+var_1 = Select(title="Please select var on x axis", value="(select)", options=[], width=245, height=50, margin=(0,0,25,0))
+var_2 = Select(title="Please select var on y axis", value="(select)", options=[], width=245, height=50, margin=(0,0,25,0))
 
-
-
+range_prompt_x = Div(text='''Please select your range for x axis''')
+range_prompt_y = Div(text='''Please select your range for y axis''')
 
 add_filter_button = Button(label="Add more filters", button_type="primary")
-
-
-##############################################################################
-# Input(s):
-#       df: dataframe to be filtered
-#       widget: the binary selection widget to be applied
-# Output(s):
-#       df: dataframe after filtering
-##############################################################################
-def apply_bi_filter(df, widget):
-
-    selected_list = widget.active
-    if selected_list:
-        column_names = [widget.labels[x] for x in selected_list]
-        df = df[df[column_names].any(axis='columns')]
-    
-    return df
-
 
 
 def plot_settings(plot, selected, plot_var_1, plot_var_2):
@@ -74,51 +44,6 @@ def plot_settings(plot, selected, plot_var_1, plot_var_2):
         plot.xaxis.axis_label = plot_var_1
         plot.yaxis.axis_label = plot_var_2
 
-##############################################################################
-# Input(s):
-#       df: dataframe to be filtered
-#       widget: the mixed (binary + categorical) selection widget to be applied
-# Output(s):
-#       df: dataframe after filtering
-##############################################################################
-# def apply_mixed_filter(df, widget):
-#     selected_list = widget.active
-
-#     for f in selected_list:        
-#         column_name = others_widget.labels[f]             
-        
-#         # Check if the filter is categorical data
-#         is_category = (df[column_name].dtype == 'category')
-
-#         if is_category:
-#             f_value = []
-#             for value_widget in widgets.children[1].children:
-#                 if value_widget.title == column_name:
-#                     f_value = value_widget.value
-#                     # f_value = [text_2_code(column_name, x) for x in f_value]
-                
-#             if f_value:
-#                 df = df[df[column_name].isin(f_value)]
-
-#         else:
-#             # df = pd.DataFrame(source_backup.data)
-#             df = df[df[column_name]]    
-
-#     return df      
-
-##############################################################################
-# Input(s):
-#       short: the shorter list of indices from widget.active
-#       long: the longer list of indices from widget.active
-# Output(s):
-#       column_name: the name of the column selected/unselected
-##############################################################################
-# def find_col_from_index(short, long):
-#     short = set(short)
-#     column_index = [x for x in long if x not in short][0]
-#     column_name = others_widget.labels[column_index]  
-
-#     return column_name
 
 # Callback for file_input
 def upload_data(attr, old, new):
@@ -132,14 +57,6 @@ def upload_data(attr, old, new):
 
     # scatter_plot_data.debug_printing()
 
-    # Function 'preprocess' in file funcs.py
-    # Returns:
-    #       df: dataframe with modified data types, 
-    #       task_filters, subspec_filters, other_filters: filters to be applied (categorical and boolean data)
-    #       numeric_var: data to be plotted (numerical data) 
-    # df, numeric_var = preprocess(df)
-
-    # print(df["task_other ( specify)"][10])
     # Update selection widget: variables to plot    
     var_1.options = scatter_plot_data.numeric_var
     var_2.options = scatter_plot_data.numeric_var
@@ -147,28 +64,38 @@ def upload_data(attr, old, new):
     # Update selection widget: filters
     first_filter_widget.options = scatter_plot_data.filter_list
 
-    # Update source data, source_backup is created for reselection purpose
-    
-
-    # source.data = df
-    # source_backup.data = df
     print('Dataset uploaded successfully')
+
+
+def convert_bool(value_list):
+    res = []
+    for x in value_list:
+        if (x == '0') or (x == '0.0'):
+            res.append(False)
+        elif (x == '1') or (x == '1.0'):
+            res.append(True)
+
+    return res if res != [] else value_list
+
 
 def apply_filter(df):
     # print(filter_widgets.children)
     for c in filter_widgets.children:
-        print(scatter_plot_data.task_values)
-        print(scatter_plot_data.subspec_values)
-        print(scatter_plot_data.filter_list)
-
-        # 
-
-        selected_filter_value = c.children[0].value
-
-        if (selected_filter_value in scatter_plot_data.task_values) or (selected_filter_value in scatter_plot_data.subspec_values):
-            df = df[df[selected_filter_value].any(axis='columns')]
-        elif selected_filter_value in scatter_plot_data.value_list:
-            df = df[df[column_name].isin(f_value)]
+        selected_filter = c.children[0].value
+        if selected_filter == '(select)':
+            continue
+        selected_filter_values = c.children[1].value
+        
+        if selected_filter in scatter_plot_data.bool_list:
+            if (selected_filter == 'tasks') or (selected_filter == 'subspec'):
+                df = df[df[selected_filter_values].any(axis='columns')]
+            else:
+                selected_filter_values = convert_bool(selected_filter_values)
+                df = df[df[selected_filter].isin(selected_filter_values)]
+        elif selected_filter in scatter_plot_data.categ_list:
+            df = df[df[selected_filter].isin(selected_filter_values)]
+        else:
+            print('Selected filter %s is neither bool nor categorical data, which should not happen', selected_filter)
 
     return df
 
@@ -183,19 +110,12 @@ def update_plot():
         button_apply.label = "Please re-select variables!"
         button_apply.button_type = "danger"  
     else:
-        button_apply.label = "Generate"
+        button_apply.label = "Generate your plot"
         button_apply.button_type = "primary"
 
         selected = pd.DataFrame(scatter_plot_data.source_backup.data)
 
-        # selected = apply_filter(selected)
-
-        # # Apply filter for task
-        # selected = apply_bi_filter(selected, task_widget)
-        # # Apply filter for subspecies
-        # selected = apply_bi_filter(selected, subspec_widget)
-        # # Apply filter for others
-        # selected = apply_mixed_filter(selected, others_widget)
+        selected = apply_filter(selected)
 
         # Set x and y range, labels, function plot_settings in funcs.py
         plot_settings(plot, selected, plot_var_1, plot_var_2)
@@ -208,43 +128,6 @@ def update_plot():
         plot.add_tools(hover)
 
 
-# Inputs:
-#       attr
-#       old: includes all values in argument 'active' in 'others_widget' CheckBox before checking, values are indices of selected filters, e.g. [0, 1]
-#       new: includes all values in argument 'active' in 'others_widget' CheckBox after checking, values are indices of selected filters, e.g. [0, 1, 2]
-# def update_filter(attr, old, new):
-
-
-    # # if add a filter
-    # if len(new) > len(old):
-    #     # Find the selected filter
-    #     column_name = find_col_from_index(old, new)
-
-    #     # Only make changes when dealing with categorical data
-    #     is_category = (source.data[column_name].dtype == 'category')
-    #     if is_category:
-        
-    #         df = pd.DataFrame(source_backup.data)
-    #         options = df[column_name]
-    #         options = list(map(str, list(set(options.tolist()))))
-    #         # options = [code_2_text(column_name, x) for x in options]
-
-    #         selectable_values = MultiChoice(title=column_name, value=[], options=options)
-    #         widgets.children[1].children.insert(0, selectable_values)
-                            
-    #     # if unselect/remove a filter
-    # else:
-    #     # Find the unselected/removed filter
-    #     column_name = find_col_from_index(new, old)
-
-    #     # Only make changes when dealing with categorical data
-    #     is_category = (source.data[column_name].dtype == 'category')
-    #     if is_category:
-            
-    #         # Delete the unselected/removed filter widget
-    #         for value_widget in widgets.children[1].children:
-    #             if value_widget.title == column_name:
-    #                 widgets.children[1].children.remove(value_widget)
 def edit_button(button, label, type):
     button.label = label
     button.button_type = type
@@ -252,8 +135,6 @@ def edit_button(button, label, type):
 
 def add_more_filters():
 
-    # new_filter_widget = Select(title='Please select your filter', value='(select)', options=new_option_list)
-    # filter_value_list, value_list = get_all_value(filter_list)
     total_options = scatter_plot_data.filter_list
     selected_options = []
     is_value_selected = True
@@ -267,15 +148,13 @@ def add_more_filters():
         edit_button(add_filter_button, "Please reselect your filter or delete!", "danger")
     else:
         edit_button(add_filter_button, "Add more filters", "primary")
-        # options = get_rest_options(total_options, selected_options)  
         options = [x for x in total_options if not x in selected_options]
         options.sort()
-        # new_option_list = list(set(total_options)-set(selected_options))
-        if options == []:
+        if options == ['(select)']:
             edit_button(add_filter_button, "No more filters", "danger")
         else:
-            new_filter_widget = Select(title='Please select your filter', value='(select)', options=options)
-            delete_button = Button(label="Delete", button_type="primary")
+            new_filter_widget = Select(title='Please select your filter', value='(select)', options=options, width=150, height=70)
+            delete_button = Button(label="Delete this filter", button_type="primary", width=50, height=30, margin=(40, 0, 0, 0), )
 
 
             new_filter_widget.on_change('value', functools.partial(add_filter_value, widget=new_filter_widget))
@@ -303,27 +182,19 @@ def delete_widget_and_button(widget):
     for c in filter_widgets.children:
         c.children[0].options.append(widget.value)
         c.children[0].options.sort()
-    # filter_widgets.children[delete_index].children.remove(filter_widgets.children[delete_index].children)
-
-
-
-# def remove_select(attr, old, new, widget):
-#     widget.options = [x for x in widget.options if not x in ['(select)']]
-    # widget.options = list(set(widget.options) - set(['(select)']))
-
 
 def add_filter_value(attr, old, new, widget):
   
    # If the filter is selected from scretch, remove the '(select)' option
-    if old != '(selected)':
+    if old == '(select)':
         widget.options.remove('(select)')
-    # If the selected filter changed, we need to update other filter's options as well
-    else:      
-        for c in filter_widgets.children:
-            if c.children[0] != widget:
-                c.children[0].options.remove(new)
+    # If the selected filter changed, need to update other filter's options as well      
+    for c in filter_widgets.children:
+        if c.children[0] != widget:
+            c.children[0].options.remove(new)
+            if old != '(select)':
                 c.children[0].options.append(old)
-                c.children[0].options.sort()
+            c.children[0].options.sort()
 
     edit_button(add_filter_button, "Add more filters", "primary")
    
@@ -331,10 +202,11 @@ def add_filter_value(attr, old, new, widget):
     options=[]
     if new == 'tasks':
         options = scatter_plot_data.task_values
-    elif new == 'subspecialities':
+    elif new == 'subspec':
         options = scatter_plot_data.subspec_values
     elif new != '(select)':
         options = scatter_plot_data.get_column_from_name(df, new)
+        options = [str(x) for x in options]
     
     widget_index = get_index_from_widget_list(new)
     if new == '(select)':
@@ -344,7 +216,7 @@ def add_filter_value(attr, old, new, widget):
         filter_widgets.children[widget_index].children[1].title = new
         filter_widgets.children[widget_index].children[1].options = options
     elif num_of_widgets == 2:
-        filter_value_widget = MultiSelect(title=new, value=[], options=options, height=70, width=200, description='Multi Select')
+        filter_value_widget = MultiSelect(title=new, value=[], options=options, height=70, width=150, description='Multi Select')
         filter_widgets.children[widget_index].children.insert(1, filter_value_widget)
 
 
@@ -353,12 +225,12 @@ file_input.on_change('value', upload_data)
 add_filter_button.on_click(add_more_filters)
 
 # Create a button widget for filters
-button_apply = Button(label="Generate", button_type="primary")
+button_apply = Button(label="Generate your plot", button_type="primary")
 button_apply.on_click(update_plot)
 
 # The first filter to be applied
-first_filter_widget = Select(title='Please select your filter', value="(select)", options=[])
-first_delete_button = Button(label="Delete", button_type="primary")
+first_filter_widget = Select(title='Please select your filter', value="(select)", options=[], width=150, height=70)
+first_delete_button = Button(label="Delete this filter", button_type="primary", width=50, height=30, margin=(40, 0, 0, 0))
 first_delete_button.on_click(functools.partial(delete_widget_and_button, widget=first_filter_widget))
 first_filter_widget.on_change('value', functools.partial(add_filter_value, widget=first_filter_widget))
 
@@ -367,5 +239,4 @@ filter_widgets = column(row(first_filter_widget, first_delete_button))
 
 
 
-curdoc().add_root(column(file_input, row(var_1, var_2), add_filter_button, filter_widgets, button_apply, plot))
-
+curdoc().add_root(row(column(upload_prompt, file_input, row(var_1, var_2), filter_widgets, add_filter_button, button_apply), plot))
